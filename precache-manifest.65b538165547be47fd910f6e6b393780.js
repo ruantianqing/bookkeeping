fe6ff731918ccd0a732be30c4e15a5dab981b41e
ruantianqing <1871348727@qq.com>
2021-03-24 11:39:45 +0800

    deploy

---
+++ precache-manifest.65b538165547be47fd910f6e6b393780.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6d241d9efc5a78fb07",
+    "url": "/bookkeeping/css/app.ef198c4c.css"
+  },
+  {
+    "revision": "94e03ba2c1b608ed9d00",
+    "url": "/bookkeeping/css/chunk-vendors.c2c417c0.css"
+  },
+  {
+    "revision": "85a44743a0d1abfc192d563295d6e465",
+    "url": "/bookkeeping/index.html"
+  },
+  {
+    "revision": "ad6d241d9efc5a78fb07",
+    "url": "/bookkeeping/js/app.d70c3f20.js"
+  },
+  {
+    "revision": "94e03ba2c1b608ed9d00",
+    "url": "/bookkeeping/js/chunk-vendors.23165292.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/bookkeeping/manifest.json"
+  },
+  {
+    "revision": "c533c26c9ecdad3aabcd124d5f8c4a82",
+    "url": "/bookkeeping/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/bookkeeping/robots.txt"
+  }
+]);
